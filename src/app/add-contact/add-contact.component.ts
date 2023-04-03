66a47f91602a6d78216e39f7165501ff6d6d0b57
JAYJP1003 <jprajapati@argusoft.com>
2023-04-03 20:54:46 +0530

    adding new column successfull

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
   styleUrls: ['./add-contact.component.css']
 })
-export class AddContactComponent {
+export class AddContactComponent implements OnInit, OnChanges{
 
+  reactiveForm: FormGroup;
+
+  public items: number[] =[1];
+  noOfTimesClicked: number = 1;
+
+  constructor() {
+    // for (let i = 0; i < this.noOfTimesClicked; i++) {
+    //   this.items.push(i);
+    // }
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      fname: new FormControl(),
+    });
+
+    
+    
+  }
+
+  // showOutput(){
+  //     // for (let i = 0; i < this.noOfTimesClicked; i++) {
+  //     //   this.items.push(i);
+  //     // }
+  //     for (let i = 0; i < 1; i++) {
+  //       this.items.push(i);
+  //     }
+  //   this.noOfTimesClicked++;
+  //   console.log(this.items);
+  // }
+
+  showOutput(){
+    
+
+      this.items.push(++this.noOfTimesClicked);
+
+    // for (let i = 0; i < 1; i++) {
+    //   this.items.push(i);
+    // }
+  console.log(this.items);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ViewContactComponent } from './view-contact/view-contact.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
